9e2e121c501fa232596c5534aa5bae3f8614c228
carterwward <Carter.Ward@uvm.edu>
2021-10-16 19:05:56 -0400

    for jayce

--- assignment_2/simulation.py
+++ assignment_2/simulation.py
@@ -5,8 +5,14 @@ class Simulation:
     def __init__(self, step: float, alpha: float, beta: float, gamma: float, rho: float, rows: int, cols: int):
         self.alpha = alpha
         self.beta = beta
-        self. gamma = gamma
+        self.gamma = gamma
         self.rho = rho
+
+        self.rows = rows
+        self.cols = cols
+
+        # 0: Hesitant, 1: Not Hesitant, 2: Unsure
+        self.rate_map = {(0, 1): (2, alpha), (1, 0): (2, beta), (1, 2): (1 ,gamma), (0, 2): (0, rho)}
 
         grid_layout = np.random.choice(3, (rows, cols))
         self.grid = self.__map_cells(grid_layout)
@@ -14,40 +20,58 @@ class Simulation:
     def __map_cells(self, grid):
         create_cell = lambda val: Cell(val)
         vmap_cells = np.vectorize(create_cell)
-        print(i, j)
+        # print(i, j)
         return vmap_cells(grid)
 
 
     def get_coordinate_list(self, i, j):
-    """gets list of lower, higher, left, and right cell
-    Args:
-        i (int): origin row index
-        j (int): origin col index
-    Returns:
-        list: list of tuples of coordinates for grid
-    """
+        """gets list of lower, higher, left, and right cell
+        Args:
+            i (int): origin row index
+            j (int): origin col index
+        Returns:
+            list: list of tuples of coordinates for grid
+        """
         if i == 0 and j == 0:
             return [(i + 1, j), (i, j + 1)]
-        if i == 0 and j == self.num_cols:
+        if i == 0 and j == self.cols:
             return [(i + 1, j), (i, j - 1)]
-        if i == self.num_rows and j == 0:
+        if i == self.rows and j == 0:
             return [(i - 1, j), (i, j + 1)]
-        if i == self.num_rows and j == self.num_cols:
+        if i == self.rows and j == self.cols:
             return [(i - 1, j), (i, j - 1)]
         if i == 0:
             return [(i, j - 1), (i, j + 1), (i + 1, j)]
-        if i == self.num_rows:
+        if i == self.rows:
             return [(i, j - 1), (i, j + 1), (i - 1, j)]
         if j == 0:
             return [(i - 1, j), (i + 1, j), (i, j + 1)]
-        if j == self.num_cols:
+        if j == self.cols:
             return [(i - 1, j), (i + 1, j), (i, j - 1)]
 
         return [(i, j - 1), (i, j + 1), (i + 1, j), (i - 1, j)]
 
     def __update_cell(self, i, j):
-        coords = self.get_coordinate_list(i, j)
-        return 1 + i + j
+        coordinate_lst = self.get_coordinate_list(i, j)
+        print(i, j, coordinate_lst)
+
+        cell = self.grid[i, j]
+
+        sum_array = np.zeros(3)
+        cell_hs = cell.hesitancy_state
+        for c in coordinate_lst:
+            c_hs = self.grid[c[0], c[1]].hesitancy_state
+            if c_hs == cell_hs:
+                continue
+
+            rate_key = (c_hs, cell_hs)
+            if rate_key not in self.rate_map.keys():
+                continue
+
+            sum_state, val = self.rate_map[rate_key]
+
+        max_index = sum_array.argmax()
+        cell.update(max_index, sum_array[max_index])
 
     def step(self):
         vupdate_cell = np.vectorize(self.__update_cell)
