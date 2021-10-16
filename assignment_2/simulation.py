f3f56acbd1a12b3afa4fd4bbe6946f48f67d2cf0
jayce <jslesar@beta.team>
2021-10-16 19:03:42 -0400

    fixed func

--- assignment_2/simulation.py
+++ assignment_2/simulation.py
@@ -7,43 +7,51 @@ class Simulation:
         self.beta = beta
         self. gamma = gamma
         self.rho = rho
+        self.rows = rows
+        self.cols = cols
 
         grid_layout = np.random.choice(3, (rows, cols))
         self.grid = self.__map_cells(grid_layout)
+        self.neighborhood_map = self.__neighborhood_map()
+        for key in self.neighborhood_map.keys():
+            print(key, self.neighborhood_map[key])
 
     def __map_cells(self, grid):
         create_cell = lambda val: Cell(val)
         vmap_cells = np.vectorize(create_cell)
-        print(i, j)
         return vmap_cells(grid)
 
+    def __neighborhood_map(self):
+        neighborhood = {}
 
-    def get_coordinate_list(self, i, j):
-    """gets list of lower, higher, left, and right cell
-    Args:
-        i (int): origin row index
-        j (int): origin col index
-    Returns:
-        list: list of tuples of coordinates for grid
-    """
-        if i == 0 and j == 0:
-            return [(i + 1, j), (i, j + 1)]
-        if i == 0 and j == self.num_cols:
-            return [(i + 1, j), (i, j - 1)]
-        if i == self.num_rows and j == 0:
-            return [(i - 1, j), (i, j + 1)]
-        if i == self.num_rows and j == self.num_cols:
-            return [(i - 1, j), (i, j - 1)]
-        if i == 0:
-            return [(i, j - 1), (i, j + 1), (i + 1, j)]
-        if i == self.num_rows:
-            return [(i, j - 1), (i, j + 1), (i - 1, j)]
-        if j == 0:
-            return [(i - 1, j), (i + 1, j), (i, j + 1)]
-        if j == self.num_cols:
-            return [(i - 1, j), (i + 1, j), (i, j - 1)]
+        def _get_coordinate_list(i, j):
+            """gets list of lower, higher, left, and right cell."""
+            if i == 0  and j == 0:
+                return [(i, j+1), (i+1, j)]
+            elif i == 0 and j == self.cols - 1:
+                return [(i, j-1), (i+1, j)]
+            elif i == self.rows - 1 and j == 0:
+                return [(i-1, j), (i, j+1)]
+            elif i == self.rows - 1 and j == self.cols - 1:
+                return [(i, j-1), (1-1, j)]
+            elif i == 0:
+                return [(i+1, j), (i, j-1), (i, j+1)]
+            elif i == self.rows - 1:
+                return [(i-1, j), (i, j-1), (i, j+1)]
+            elif j == 0:
+                return [(i, j+1), (i-1, j), (i+1, j)]
+            elif j == self.cols - 1:
+                return [(i, j-1), (i-1, j), (i+1, j)]
+            else:
+                return [(i, j-1), (i, j+1), (i+1, j), (i-1, j)]
 
-        return [(i, j - 1), (i, j + 1), (i + 1, j), (i - 1, j)]
+        for i in range(self.rows):
+            for j in range(self.cols):
+                # print(i, j)
+                neighborhood[(i, j)] = _get_coordinate_list(i, j)
+
+        return neighborhood
+
 
     def __update_cell(self, i, j):
         coords = self.get_coordinate_list(i, j)
@@ -53,13 +61,11 @@ class Simulation:
         vupdate_cell = np.vectorize(self.__update_cell)
         copy_grid = np.fromfunction(vupdate_cell, self.grid.shape)
         self.grid = copy_grid
-        print(self.grid)
 
 
 
     def update_grid(self):
         pass
 
-s = Simulation(10, 10, 10, 10, 10, 2, 2)
+s = Simulation(10, 10, 10, 10, 10, 3, 3)
 
-s.step()
