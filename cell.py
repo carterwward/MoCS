70d79d73441409070704891b6f1774fd210a2301
jayce <jslesar@beta.team>
2021-10-16 16:38:12 -0400

    cell class initial push

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
 """Cell Class for Vaccine Hesitancy Model"""
 from enum import Enum
+import numpy as np
 
 
 class HesitancyState(Enum):
@@ -9,16 +10,41 @@ class HesitancyState(Enum):
     UNSURE = 2
 
 
-class OpenMindedness(Enum):
-    """Ranges represented by tuple for open mindedness factor."""
-    HESITANT = ()
-    NOT_HESITANT = ()
-    UNSURE = ()
-
-
 class Cell:
-    def __init__(self, hesitancy_state: HesitancyState, row: int, col: int):
-        pass
+    def __init__(self, hesitancy_state: HesitancyState, row: int, column: int):
+        """Initialize instance of Cell class, representing a person with a given HesitancyState.
 
-    def update(self):
-        pass
+        Args:
+            hesitancy_state (HesitancyState): initial HesitancyState of person
+            row (int): row in grid
+            column (int): column in grid
+        """
+        self.hesitancy_state = hesitancy_state
+        self.row = row
+        self.column = column
+        self.open_mindedness = None
+
+        self.get_open_mindedness_value()  # dependent on self.open_mindedness
+
+    def get_open_mindedness_value(self):
+        """get open_mindedness value from the current self.hesitancy_state."""
+        if self.hesitancy_state == HesitancyState.HESITANT:
+            self.open_mindedness = np.random.uniform(0.2, 0.5)
+
+        elif self.hesitancy_state == HesitancyState.NOT_HESITANT:
+            self.open_mindedness = np.random.uniform(0.2, 0.5)
+
+        elif self.hesitancy_state == HesitancyState.UNSURE:
+            self.open_mindedness = np.random.uniform(0, 0.2)
+
+    def open_mindedness_noise(self):
+        """Random noise for open_mindedness state."""
+        self.open_mindedness += np.random.uniform(-0.2, 0.2)
+
+    def update(self, max_state_in_neighborhood: HesitancyState, new_state_influence: float):
+        """Update the cell based on value calculated from it's neighbords in simulation.py"""
+        if new_state_influence > self.open_mindedness:
+            self.hesitancy_state = max_state_in_neighborhood
+            self.get_open_mindedness_value()
+
+        self.open_mindedness_noise()
