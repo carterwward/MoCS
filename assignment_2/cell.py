4f63d0b3ab9c4d50a38baaf09083ce2e9aa00c94
jayce <jslesar@beta.team>
2021-10-16 17:12:55 -0400

    remive Enum in favor of int

--- assignment_2/cell.py
+++ assignment_2/cell.py
@@ -1,17 +1,10 @@
 """Cell Class for Vaccine Hesitancy Model"""
-from enum import Enum
+from typing import Union
 import numpy as np
 
 
-class HesitancyState(Enum):
-    """Numerical representation of state."""
-    HESITANT = 0
-    NOT_HESITANT = 1
-    UNSURE = 2
-
-
 class Cell:
-    def __init__(self, hesitancy_state: HesitancyState, row: int, column: int):
+    def __init__(self, hesitancy_state: int):
         """Initialize instance of Cell class, representing a person with a given HesitancyState.
 
         Args:
@@ -20,28 +13,26 @@ class Cell:
             column (int): column in grid
         """
         self.hesitancy_state = hesitancy_state
-        self.row = row
-        self.column = column
         self.open_mindedness = None
 
         self.get_open_mindedness_value()  # dependent on self.open_mindedness
 
     def get_open_mindedness_value(self):
         """get open_mindedness value from the current self.hesitancy_state."""
-        if self.hesitancy_state == HesitancyState.HESITANT:
+        if self.hesitancy_state == 0:
             self.open_mindedness = np.random.uniform(0.2, 0.5)
 
-        elif self.hesitancy_state == HesitancyState.NOT_HESITANT:
+        elif self.hesitancy_state == 1:
             self.open_mindedness = np.random.uniform(0.2, 0.5)
 
-        elif self.hesitancy_state == HesitancyState.UNSURE:
+        elif self.hesitancy_state == 2:
             self.open_mindedness = np.random.uniform(0, 0.2)
 
     def open_mindedness_noise(self):
         """Random noise for open_mindedness state."""
         self.open_mindedness += np.random.uniform(-0.2, 0.2)
 
-    def update(self, max_state_in_neighborhood: HesitancyState, new_state_influence: float):
+    def update(self, max_state_in_neighborhood: int, new_state_influence: float):
         """Update the cell based on value calculated from it's neighbords in simulation.py"""
         if new_state_influence > self.open_mindedness:
             self.hesitancy_state = max_state_in_neighborhood
@@ -56,11 +47,11 @@ class Cell:
         Returns:
             tuple: (r, g, b) tuple determined by the self.hesitancy_state
         """
-        if self.hesitancy_state == HesitancyState.HESITANT:
+        if self.hesitancy_state == 0:
             return (255, 0, 0)  # red
 
-        elif self.hesitancy_state == HesitancyState.NOT_HESITANT:
+        elif self.hesitancy_state == 1:
             return (0, 128, 0)  # green
 
-        elif self.hesitancy_state == HesitancyState.UNSURE:
+        elif self.hesitancy_state == 2:
             return (255, 255, 255)  # white
